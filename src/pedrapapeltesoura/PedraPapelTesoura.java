52f8eb27b173c5d1536b3752fb7adf8435834e76
EnzoAndrade2001 <182220019@senacinformatica.br>
2023-11-17 20:11:35 -0300

    fksdkf

---
+++ src/pedrapapeltesoura/PedraPapelTesoura.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pedrapapeltesoura;
+
+/**
+ *
+ * @author 182220019
+ */
+public class PedraPapelTesoura {
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
